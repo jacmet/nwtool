6c6dedf41fecee821a679ca287c9648f5d7b462c
Peter Korsgaard <jacmet@sunsite.dk>
2008-02-01 12:37:07 +0000

    nwtool: Add USB getters and WIP interrupt pipe parsing.

--- src/nwtool-usb.c
+++ src/nwtool-usb.c
@@ -79,7 +79,7 @@ static int nw_usb_send(HIDInterface *hid, void *data, int len)
 static int nw_usb_recv(HIDInterface *hid, void *data)
 {
 	return hid_interrupt_read(hid, 2 | USB_ENDPOINT_IN, data,
-				  NWUSB_PACKETSIZE, 100000);
+				  NWUSB_PACKETSIZE, 1000);
 }
 
 static int nw_usb_calibrate(HIDInterface *hid, int enable)
@@ -165,6 +165,67 @@ static int nw_usb_restore_factory_defaults(HIDInterface *hid)
 	return nw_usb_send(hid, buf, sizeof(buf));
 }
 
+static void nw_usb_parse(unsigned char *buf)
+{
+	unsigned short data16;
+	unsigned int data32;
+
+	data16 = (buf[3] << 8) + buf[4];
+	data32 = (buf[3] << 24) + (buf[4] << 16) + (buf[5] << 8) + buf[6];
+
+	switch (buf[0]) {
+	case 'C':
+		switch (buf[2]) {
+		case 0x10:
+			printf("Model = %u\n", data16);
+			break;
+		case 0x11:
+			printf("Firmware = %u.%02u\n", buf[3], buf[4]);
+			break;
+		case 0x12:
+			printf("Serial = %u\n", data32);
+			break;
+		case 0x20:
+			printf("HW caps = 0x%02x\n", buf[3]);
+			break;
+		case 0x21:
+			printf("Calibration mode = %u\n", buf[3]);
+			break;
+		case 0x30:
+			printf("Right click delay = %u ms\n", buf[3] * 10);
+			break;
+		case 0x31:
+			printf("Double click time = %u ms\n", buf[3] * 10);
+			break;
+		case 0x32:
+			printf("Report mode = 0x%02x\n", buf[3]);
+			break;
+		case 0x33:
+			printf("Drag threshold = %u\n", data16);
+			break;
+		case 0x34:
+			printf("Buzzer time = %u ms\n", buf[3] * 10);
+			break;
+		case 0x35:
+			printf("Buzzer tone = %u\n", buf[3]);
+			break;
+		case 0x40:
+			printf("Calibration key = %u\n", buf[3]);
+			break;
+		case 0x41:
+			printf("Calibration presses = %u\n", buf[3]);
+			break;
+		default:
+			printf("unknown 'C' packet\n");
+		}
+		break;
+
+	default:
+		printf("Unknown packet (%u)\n", buf[0]);
+		break;
+	}
+}
+
 static void nw_usb_process(HIDInterface *hid)
 {
 	unsigned char buf[NWUSB_PACKETSIZE];
@@ -175,7 +236,73 @@ static void nw_usb_process(HIDInterface *hid)
 		for (i=0; i<10; i++)
 			printf("%02x ", buf[i]);
 		printf("\n");
+		nw_usb_parse(buf);
 	}
+}
+
+static int nw_usb_get_model(HIDInterface *hid)
+{
+	unsigned char buf[] = { 'C', 1, 0x10 };
+	int ret;
+
+	ret = nw_usb_send(hid, buf, sizeof(buf));
+	if (ret)
+		return ret;
+
+	nw_usb_process(hid);
+	return 0;
+}
+
+static int nw_usb_get_firmware(HIDInterface *hid)
+{
+	unsigned char buf[] = { 'C', 1, 0x11 };
+	int ret;
+
+	ret = nw_usb_send(hid, buf, sizeof(buf));
+	if (ret)
+		return ret;
+
+	nw_usb_process(hid);
+	return 0;
+}
+
+static int nw_usb_get_serial(HIDInterface *hid)
+{
+	unsigned char buf[] = { 'C', 1, 0x12 };
+	int ret;
+
+	ret = nw_usb_send(hid, buf, sizeof(buf));
+	if (ret)
+		return ret;
+
+	nw_usb_process(hid);
+	return 0;
+}
+
+static int nw_usb_get_calibration_key(HIDInterface *hid)
+{
+	unsigned char buf[] = { 'C', 1, 0x40 };
+	int ret;
+
+	ret = nw_usb_send(hid, buf, sizeof(buf));
+	if (ret)
+		return ret;
+
+	nw_usb_process(hid);
+	return 0;
+}
+
+static int nw_usb_get_calibration_presses(HIDInterface *hid)
+{
+	unsigned char buf[] = { 'C', 1, 0x41 };
+	int ret;
+
+	ret = nw_usb_send(hid, buf, sizeof(buf));
+	if (ret)
+		return ret;
+
+	nw_usb_process(hid);
+	return 0;
 }
 
 void nw_usb_test(void)
@@ -194,8 +321,13 @@ void nw_usb_test(void)
 		fprintf(stderr, "Error sending calibrate cmd (%d)\n", ret);
 	}
 #endif
+	nw_usb_get_model(hid);
+	nw_usb_get_firmware(hid);
+	nw_usb_get_serial(hid);
+	nw_usb_get_calibration_key(hid);
+	nw_usb_get_calibration_presses(hid);
 
-	ret = nw_usb_set_buzzer_time(hid, 100);
+	ret = nw_usb_set_buzzer_time(hid, 50);
 	if (ret) {
 		fprintf(stderr, "Error setting buzzer time (%d)\n", ret);
 	}
